7451fedc3ec56ac1f62ac179b5979a3031451af4
surendra dj h <98631400+Surendradjh@users.noreply.github.com>
2025-02-26 14:35:46 +0530

    Add files via upload

---
+++ Spam Detection/spam_streamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+st.title('E-Mail Spam Detection')
+with open(r"spam_detection.pkl",'rb') as file:
+    model=pickle.load(file)
+
+with open(r"Text_converter.pkl","rb") as file:
+    tr=pickle.load(file)
+
+E_mail=st.text_input("Enter E-Mail Title")
+if st.button('Predict'):
+    a=tr.transform([E_mail])
+    st.write(a)
+    a=model.predict(a)
+    if a == 'ham':
+        st.write("It's not Spam message")
+    else:
+        st.write("It's a spam Message please avoid this Message ")
