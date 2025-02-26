5dee032700bfc7c356c969cbcd144860d8ee44c3
surendra dj h <98631400+Surendradjh@users.noreply.github.com>
2025-02-26 14:46:04 +0530

    Update spam_streamlit.py

--- Spam Detection/spam_streamlit.py
+++ Spam Detection/spam_streamlit.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pickle
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 
 st.title('E-Mail Spam Detection')
 with open(r"spam_detection.pkl",'rb') as file:
